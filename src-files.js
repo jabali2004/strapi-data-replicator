63a8e1041bd44b92de4c46b59c1be8bf464f5d94
jabali2004 <jabali2004@users.noreply.github.com>
2023-10-24 05:44:05 +0000

    deploy: e633c5c6208156c5c76b5d7cf853fbdb648e87bd

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"strapi_data_replicator":["",[["modules",[["commands",[],["init.rs","migrate.rs","replicate.rs","tables.rs"]],["services",[["databases",[],["mongodb.rs","mysql.rs"]]],["databases.rs","utils.rs"]],["types",[],["build_info.rs","cli.rs","config.rs","package_json.rs"]]],["commands.rs","services.rs","types.rs"]]],["consts.rs","main.rs","modules.rs"]]\
+}');
+createSrcSidebar();
