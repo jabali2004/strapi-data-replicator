d089fb7f27b9b783ce270b3d8ef6089220380c44
jabali2004 <jabali2004@users.noreply.github.com>
2023-12-31 21:07:43 +0000

    deploy: 08e50ee8396bcb49b56cf6cb0c6e036c8d92b862

---
+++ trait.impl/structopt/trait.StructOpt.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"strapi_data_replicator":[["impl StructOpt for <a class=\"struct\" href=\"strapi_data_replicator/modules/types/cli/struct.Cli.html\" title=\"struct strapi_data_replicator::modules::types::cli::Cli\">Cli</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
